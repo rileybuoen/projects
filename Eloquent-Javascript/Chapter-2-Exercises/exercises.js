9c7a44523dd6c81d560f216dc97c881cf94fa907
Riley Buoen <58043852+rbuoen@users.noreply.github.com>
2021-06-05 20:20:13 -0700

    added exercises

---
+++ Eloquent-Javascript/Chapter-2-Exercises/exercises.js
@@ -0,0 +1,60 @@
+/*
+Exercises from Chapter 2
+of Eloquent Javascript
+June 5, 2021
+*/
+
+
+// Looping a Triangle
+
+let triangle = "#";
+
+while (triangle.length < 8) {
+    console.log(triangle)
+    triangle += "#";
+}
+
+// FizzBuzz
+
+for (let num = 0; num < 101; num++) {
+    if ((num % 5) && (num % 3) === 0) {
+        console.log("FizzBuzz")
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num)
+    }
+}
+
+
+// Chessboard
+
+const white = " ";
+const black = "#";
+let size = 8;
+
+for (let i = 0; i < size; i++) {
+    if (i % 2 === 0) {
+        let row = "";
+        for (let j = 0; j < size; j++) {
+            if (j % 2 === 0) {
+                row += white;
+            } else {
+                row += black;
+            }
+        }
+        console.log(row);
+    } else {
+        let row = "";
+        for (let k = 0; k < size; k++) {
+            if (k % 2 === 0) {
+                row += black;
+            } else {
+                row += white;
+            }
+        }
+        console.log(row);
+    }
+}
