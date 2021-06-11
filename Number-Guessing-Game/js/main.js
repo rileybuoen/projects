fddf51f0278c39b5b3993ee124899cf042f394e7
Riley <buoenriley@gmail.com>
2021-06-11 12:22:33 -0700

    added turn counter

--- Number-Guessing-Game/js/main.js
+++ Number-Guessing-Game/js/main.js
@@ -4,36 +4,58 @@ class Game {
     constructor() {
         // the guess
         this.guess = 0;
+
         // the random number
         this.num = Math.floor(Math.random() * 100);
         console.log(this.num);
+
+        // turn counter
+        this.counter = 10;
+
+        // game state
+        this.gameOver = false;
     }
     getGuess() {
-        // gets the input and converts it from a string to and integer
-        this.guess = parseInt(document.getElementById("inputBox").value, 10);
-        console.log(this.guess);
+        if (this.gameOver === false) {
+            // gets the input and converts it from a string to and integer
+            this.guess = parseInt(document.getElementById("inputBox").value, 10);
+            console.log(this.guess);
 
-        // calls getHint
-        game.getHint(this.guess);
+            // calls getHint
+            game.getHint(this.guess);
+        } else {
+            console.log("The Game is Over")
+        }
     }
     getHint(guess) {
         // compares the guess to the random number
-        if (guess > this.num) {
+        if (this.counter > 1 && guess > this.num) {
+            this.counter--;
+
             console.log("Guess too high!");
-            return "Guess too high!";
-        } else if (guess < this.num) {
+            console.log(this.getTurnCount());
+
+        } else if (this.counter > 1 && guess < this.num) {
+            this.counter--;
+
             console.log("Guess too low!");
-            return "Guess too low!";
+            console.log(this.getTurnCount());
+
         } else {
             game.guessCheck(guess);
         } 
     }
+    getTurnCount() {
+        return "Turns left: " + this.counter.toString();
+    }
     guessCheck(guess) {
         // checks if guess is equal to the random number
-        if (guess === this.num) {
-            console.log("You Win!")
+        if (this.counter > 0 && guess === this.num) {
+            console.log("You Win!");
+            this.gameOver = true;
         } else {
-            console.log("Try Again")
+            console.log("Out of turns!");
+            this.gameOver = true;
         }
     }
 }
